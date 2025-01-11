aa3db4e6a13eef60f1b7affac8e0446dd01ca3d0
Marius Odediran <themariusodediran@gmail.com>
2025-01-12 00:02:15 +0100

    made some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'phones': {'min': '320px', 'max': '500px'}
+      }
+    },
+    fontFamily: {
+      Inter: ['Inter', 'serif']
+    }
   },
   plugins: [],
 }
